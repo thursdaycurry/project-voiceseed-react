227ab5fc8b94ec3d0cd7aa61b149a207e78d8eb3
thursdaycurry <thursdaycurry@gmail.com>
2025-04-17 01:18:44 +0900

    feat(header): create simple header

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header className="flex bg-green justify-between w-full mx-auto px-10 py-2 h-[60px] items-center">
+      {/* Logo part */}
+      <div className="font-bold text-xl">VoiceSeed</div>
+
+      {/* Center part */}
+      <nav className="flex space-x-10">
+        <Link to="/">Home</Link>
+        <Link to="/how-it-works">How It works</Link>
+        <Link to="/pricing">Pricing</Link>
+      </nav>
+      {/* User part */}
+      <div className="flex space-x-10 ">
+        <div>Login</div>
+      </div>
+    </header>
+  );
+}
